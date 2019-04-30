814f0e2433fc6cb2273d39e76c614e05cab1b9f8
Deidra22 <Deidra22@users.noreply.github.com>
2019-04-29 20:58:00 -0700

    for loop and do while loop

---
+++ js/loops.js
@@ -0,0 +1,20 @@
+//loops
+var fruits = ['apple', 'orange', 'strawberry', 'pear', 'kiwi'];
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+//for loop
+// for (var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// do while
+var index = 0;
+do {
+    console.log(fruits[index]);
+    index++;
+} while ( index < fruits.length);
