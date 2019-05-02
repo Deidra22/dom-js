5db706591882df1c6f744a3ca91391484efc2151
Deidra22 <Deidra22@users.noreply.github.com>
2019-05-02 12:48:03 -0700

    half done second half not understanding

--- js/dom-event-listener.js
+++ js/dom-event-listener.js
@@ -5,14 +5,14 @@ btn[0].style.padding = '10px';
 btn[0].style.fontSize = '22px';
 
 //add Event Listener
-btnUsingSelector.addEventListener('click', function (){
-    console.log('Button was clicked from JS!');
-});
+// btnUsingSelector.addEventListener('click', function (){
+//     console.log('Button was clicked from JS!');
+// });
 
-btnUsingSelector.addEventListener('mouseover', function (){
-    var btnText = event.target.innerHTML;
-    alert('Mouse over on the ' + btnText + ' button.');
-});
+// btnUsingSelector.addEventListener('mouseover', function (){
+//     var btnText = event.target.innerHTML;
+//     alert('Mouse over on the ' + btnText + ' button.');
+// });
 
 //cancel button
 var cancelBtn = document.getElementsByClassName('cancel');
@@ -26,26 +26,31 @@ cancelbtnUsingSelector.addEventListener('click', function(){
     var response = confirm('confirm cancel');
     console.log(response);
     // if user confirms do something = response value true
+    if (response = true){
+        alert('Cancel Confirmed!');
+    } else {
+        alert('Did not Cancel!');
+    }
     // if user cancels do something = response value false
 });
 
-document.addEventListener('keypress', function (event){
-    console.log(event.key);
-    console.log(event.keyCode);
-});
+// document.addEventListener('keypress', function (event){
+//     console.log(event.key);
+//     console.log(event.keyCode);
+// });
 
-document.addEventListener('keydown', function (event){
-    console.log(event.key);
-    console.log(event.keyCode);
+// document.addEventListener('keydown', function (event){
+//     console.log(event.key);
+//     console.log(event.keyCode);
     
-});
+// });
 
-document.addEventListener('keyup', function (event){
-    console.log(event.key);
-    console.log(event.keyCode);
+// document.addEventListener('keyup', function (event){
+//     console.log(event.key);
+//     console.log(event.keyCode);
     
-});
+// });
 
-document.addEventListener('scroll', function(event){
-    console.log(event);
-});
+// document.addEventListener('scroll', function(event){
+//     console.log(event);
+// });
