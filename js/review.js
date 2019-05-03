480a3dc394dba7e3ed8548930c775a2fce54a2ed
Deidra22 <Deidra22@users.noreply.github.com>
2019-05-02 20:56:31 -0700

    added hp

--- js/review.js
+++ js/review.js
@@ -1,11 +1,36 @@
 var user = {
 
 };
-
 var name = prompt('Enter Name');
+// if (name === '' || parseInt(name) || name === ' '){
+//     alert('Please Enter a name');
+// }else{
+//     user.name = name;
+//     alert('Hello '+ name);
+// }
+// console.log(user);
 
-if (name === '' || parseInt(name)){
-    alert('Please Enter a name');
+if (name && isNaN(name)){
+    user.name = name;
+    var hp = prompt('Enter your HP:');
+    console.log(hp);
+    console.log(isNaN(hp));
+    if (hp && !isNaN(parseInt(hp))){
+        user.power = parseInt(hp);
+    } else{
+        alert('Enter valid HP');
+    }
 }else{
-    alert('Hello' +' '+ name);
+    alert('Enter a Valid Name');
 }
+
+console.log(user);
+// console.log({name});
+// console.log(!name);
+// console.log(!isNaN(name));
+
+//ternary
+// (name === '' || name === ' ' || parseInt(name))?
+//     alert('Please enter a name') :
+//     user.name = name;
+
